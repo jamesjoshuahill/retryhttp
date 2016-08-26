218e542e60f120e9f4ee0ff0e79263cb441ce64b
Josh Hill <jhill@pivotal.io>
2016-08-26 16:54:08 +0100

    Add test to document minimum number of retries
    ExponentialRetryPolicy will retry at least 3 times with 7 seconds of
    delay

--- retry_policy_test.go
+++ retry_policy_test.go
@@ -19,59 +19,84 @@ var _ = Describe("ExponentialRetryPolicy", func() {
 		keepRetrying bool
 	)
 
-	BeforeEach(func() {
-		policy = retryhttp.ExponentialRetryPolicy{
-			Timeout: 5 * time.Minute,
-		}
-
-		attempts = 0
-	})
-
-	JustBeforeEach(func() {
-		delay, keepRetrying = policy.DelayFor(attempts)
-	})
-
 	type row struct {
 		attempts     uint
 		delay        time.Duration
 		keepRetrying bool
 	}
 
-	for _, row := range []row{
-		{0, 0 * time.Second, true},
-		{1, 1 * time.Second, true},
-		{2, 2 * time.Second, true},
-		{3, 4 * time.Second, true},
-		{4, 8 * time.Second, true},
-		{5, 16 * time.Second, true},
-		{6, 16 * time.Second, true},
-		{7, 16 * time.Second, true},
-		{8, 16 * time.Second, true},
-		{9, 16 * time.Second, true},
-		{20, 16 * time.Second, true},
-		{21, 0, false},
-		{22, 0, false},
-	} {
-		row := row
+	var testAttempts = func(rows []row) {
+		for _, row := range rows {
+			row := row
 
-		Context(fmt.Sprintf("after %d failed attempts", row.attempts), func() {
-			BeforeEach(func() {
-				attempts = row.attempts
-			})
-
-			It(fmt.Sprintf("returns a %s delay", row.delay), func() {
-				Expect(delay).To(Equal(row.delay))
-			})
-
-			if row.keepRetrying {
-				It("keeps retrying", func() {
-					Expect(keepRetrying).To(BeTrue())
+			Context(fmt.Sprintf("after %d failed attempts", row.attempts), func() {
+				BeforeEach(func() {
+					attempts = row.attempts
 				})
-			} else {
-				It("gives up", func() {
-					Expect(keepRetrying).To(BeFalse())
+
+				It(fmt.Sprintf("returns a %s delay", row.delay), func() {
+					Expect(delay).To(Equal(row.delay))
 				})
-			}
-		})
+
+				if row.keepRetrying {
+					It("keeps retrying", func() {
+						Expect(keepRetrying).To(BeTrue())
+					})
+				} else {
+					It("gives up", func() {
+						Expect(keepRetrying).To(BeFalse())
+					})
+				}
+			})
+		}
 	}
+
+	JustBeforeEach(func() {
+		delay, keepRetrying = policy.DelayFor(attempts)
+	})
+
+	Context("when the timeout is 1 second", func() {
+		BeforeEach(func() {
+			policy = retryhttp.ExponentialRetryPolicy{
+				Timeout: 1 * time.Second,
+			}
+
+			attempts = 0
+		})
+
+		testAttempts([]row{
+			{0, 0 * time.Second, true},
+			{1, 1 * time.Second, true},
+			{2, 2 * time.Second, true},
+			{3, 4 * time.Second, true},
+			{4, 0, false},
+			{5, 0, false},
+		})
+	})
+
+	Context("when the timeout is 5 minutes", func() {
+		BeforeEach(func() {
+			policy = retryhttp.ExponentialRetryPolicy{
+				Timeout: 5 * time.Minute,
+			}
+
+			attempts = 0
+		})
+
+		testAttempts([]row{
+			{0, 0 * time.Second, true},
+			{1, 1 * time.Second, true},
+			{2, 2 * time.Second, true},
+			{3, 4 * time.Second, true},
+			{4, 8 * time.Second, true},
+			{5, 16 * time.Second, true},
+			{6, 16 * time.Second, true},
+			{7, 16 * time.Second, true},
+			{8, 16 * time.Second, true},
+			{9, 16 * time.Second, true},
+			{20, 16 * time.Second, true},
+			{21, 0, false},
+			{22, 0, false},
+		})
+	})
 })
